96ec9f9637d5d1edd009fc22049c677bd18560a5
edjunma <edjunma@gmail.com>
2023-07-28 06:00:49 -0400

    add bio, framer animation to profile

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
-import { Container, Box, Heading } from '@chakra-ui/react'
+import NextLink from 'next/link'
+import { Link, Container, Box, Heading, Button, List, ListItem, useColorModeValue } from '@chakra-ui/react'
+import { ChevronRightIcon, EmailIcon } from '@chakra-ui/icons'
+import Section from '../components/section'
+import Paragraph from '../components/paragraph'
+import Image from 'next/image'
 const Page = () => {
   return (
     <Container>
-      <Box borderRadius="lg" mb={6} p={3} textAlign="center" bg="red">
+      <Box borderRadius="lg" mb={6} p={3} textAlign="center" bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+        css={{ backdropFilter: 'blur(10px)' }}>
       Hello, I&apos;m a Full-Stack Developer based in NYC!
     </Box>
 
@@ -13,7 +19,56 @@ const Page = () => {
         </Heading>  
         <p>Full-Stack Developer ( Developer / Designer / Photographer )</p>
       </Box>
-    </Box>
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          textAlign="center"
+        >
+          <Box
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            w="100px"
+            h="100px"
+            display="inline-block"
+            borderRadius="full"
+            overflow="hidden"
+          >
+            <Image
+              src="/images/profile.jpg"
+              alt="Profile image"
+              borderRadius="full"
+              width="100"
+              height="100"
+            />
+          </Box>
+        </Box>
+      </Box>
+
+      <Section delay={0.1}>
+        <Heading as="h3" variant="section-title">
+          Work
+        </Heading>
+        <Paragraph>
+          Edmond is a freelance and a full-stack developer based in NYC with a
+          passion for building digital services/stuff he wants. He has a knack
+          for all things launching products, from planning and designing all the
+          way to solving real-life problems with code. When not online, he loves
+          hanging out with his dogs and taking photos. 
+        </Paragraph>
+        <Box align="center" my={4}>
+          <Button
+            as={NextLink}
+            href="/works"
+            scroll={false}
+            rightIcon={<ChevronRightIcon />}
+            colorScheme="teal"
+          >
+            My portfolio
+          </Button>
+        </Box>
+      </Section>
     </Container>
     )
 }
