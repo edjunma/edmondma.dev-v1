1ea615279c242f159d04d6ded71cdaf9d295d8f2
edjunma <edjunma@gmail.com>
2023-10-04 01:35:09 -0400

    update

--- pages/index.js
+++ pages/index.js
@@ -66,7 +66,7 @@ const Page = () => {
             Full-Stack Developer focused on front-end technologies with a passion for building digital products & cozy aesthetic websites. When offline, I love to spend time with my dogs {' '}
             <Link href="https://www.instagram.com/heihei.and.elsa/" target="_blank">
               @heihei.and.elsa
-            </Link>, daily exercise because health is wealth, cook various cuisines, and these days fiddle with VR Development, photography, and video editing.
+            </Link>, exercise because health is wealth, cook various cuisines, and these days fiddle with VR Development, photography, and video editing.
           </Paragraph>
           <Box align="center" my={4}>
             <Button
